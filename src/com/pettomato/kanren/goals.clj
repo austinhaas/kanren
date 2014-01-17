16ad42000ae05cc21390b8f479a4ff04e7329b09
Austin Haas <austin@pettomato.com>
2014-01-16 16:39:41 -0800

    Add goals.

---
+++ src/com/pettomato/kanren/goals.clj
@@ -0,0 +1,36 @@
+(ns com.pettomato.kanren.goals
+  (:refer-clojure :exclude [==])
+  (:require
+   [com.pettomato.kanren.llist :refer (empty-llist)]
+   [com.pettomato.kanren.cKanren :refer (!= == fresh conde)]))
+
+(defn emptyo [l]
+  (== l empty-llist))
+
+(defn conso [a d l]
+  (== [a d] l))
+
+(defn membero [x l]
+  (fresh [head tail]
+    (conso head tail l)
+    (conde
+      [(== x head)]
+      [(!= x head) (membero x tail)])))
+
+(defn appendo [x y z]
+  (conde
+    [(emptyo x) (== y z)]
+    [(fresh [a d r]
+       (conso a d x)
+       (conso a r z)
+       (appendo d y r))]))
+
+(defn nonmembero
+  "A relation where l is a collection, such that l does not contain x."
+  [x l]
+  (conde
+    [(emptyo l)]
+    [(fresh [head tail]
+       (conso head tail l)
+       (!= x head)
+       (nonmembero x tail))]))
