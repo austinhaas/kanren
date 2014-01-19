3b1d60c4235acdbcfd6442c9e916fbd534438a47
Austin Haas <austin@pettomato.com>
2014-01-18 23:20:31 -0800

    rKanren

---
+++ src/com/pettomato/kanren/rKanren.clj
@@ -0,0 +1,139 @@
+(ns com.pettomato.kanren.rKanren
+  (:refer-clojure :exclude [== conj disj])
+  (:require
+   [com.pettomato.kanren.muKanren :as mu]
+   [com.pettomato.kanren.cKanren :as c]))
+
+(def pair? mu/pair?)
+
+(def mzero mu/mzero)
+(def choice mu/choice)
+
+(defn get-rank [a-inf]
+  (get (meta a-inf) ::rank -1))
+
+(defn set-rank [a-inf r]
+  (vary-meta a-inf assoc ::rank r))
+
+(defn inc-rank [a-inf]
+  (if (contains? (meta a-inf) ::rank)
+    (vary-meta a-inf update-in [::rank] inc)
+    a-inf))
+
+(defn add-rank [a-inf r]
+  (vary-meta a-inf update-in [::rank] + r))
+
+(defmacro case-inf
+  [e _ e0 [f'] e1 [a'] e2 [a f] e3]
+  `(let [a# ~e]
+     (cond
+      (false? a#) (inc-rank ~e0)
+      (fn? a#) (let [~f' a#] (inc-rank ~e1))
+      (not (and (pair? a#) (fn? (second a#))))
+      (let [~a' a#]
+        (inc-rank ~e2))
+      :else (let [~a (first a#)
+                  ~f (second a#)]
+              (inc-rank ~e3)))))
+
+(defn mplus [a-inf f]
+  (case-inf a-inf
+            []     f
+            [f']   (let [f'-rank (get-rank f')
+                         f-rank  (get-rank f)]
+                     (if (< f'-rank f-rank)
+                       (set-rank (fn [] (mplus (f') f))
+                                 f'-rank)
+                       (set-rank (fn [] (mplus (f)  f'))
+                                 f-rank)))
+            [a]    (choice a f)
+            [a f'] (choice
+                    a
+                    (let [f'-rank (get-rank f')
+                          f-rank  (get-rank f)]
+                      (if (< f'-rank f-rank)
+                        (set-rank (fn [] (mplus (f') f))
+                                  f'-rank)
+                        (set-rank (fn [] (mplus (f)  f'))
+                                  f-rank))))))
+
+(defn bind [a-inf g]
+  (case-inf a-inf
+            []    mzero
+            [f]   (set-rank (fn [] (bind (f) g))
+                            (get-rank f))
+            [a]   (g a)
+            [a f] (mplus (g a) (set-rank (fn [] (bind (f) g))
+                                         (get-rank f)))))
+
+(defn disj [g1 g2]
+  (fn [a]
+    (let [a' (inc-rank a)
+          x1 (g1 a')
+          x2 (g2 a')
+          r1 (get-rank x1)
+          r2 (get-rank x2)]
+      (set-rank
+       (mplus x1 x2)
+       (min r1 r2)))))
+
+(defn conj [g1 g2]
+  (fn [a]
+    (let [x (g1 a)]
+      (set-rank
+       (bind x g2)
+       (get-rank x)))))
+
+(defmacro Zzz [g]
+  `(fn [a#]
+     (set-rank
+      (fn [] (~g a#))
+      (get-rank a#))))
+
+(defmacro conj+
+  ([g] `(Zzz ~g))
+  ([g & gs]
+     `(conj (Zzz ~g) (conj+ ~@gs))))
+
+(defmacro disj+
+  ([g] `(Zzz ~g))
+  ([g & gs]
+     `(disj (Zzz ~g) (disj+ ~@gs))))
+
+(defmacro conde
+  [& clauses]
+  `(disj+
+    ~@(for [c clauses]
+        `(conj+ ~@c))))
+
+(defmacro condr
+  [& clauses]
+  `(disj+
+    ~@(for [[r & c] clauses]
+        `(fn [a#]
+           ((conj+ ~@c) (add-rank a# ~r))))))
+
+(defn call:fresh [f]
+  (fn [{:keys [i] :as pkg}]
+    (let [pkg' (update-in pkg [:i] inc)]
+      ((f (c/lvar i)) pkg'))))
+
+(defmacro fresh
+  [[& vars] & gs]
+  (if (empty? vars)
+    `(conj+ ~@gs)
+    `(call:fresh (fn [~(first vars)] (fresh ~(rest vars) ~@gs)))))
+
+(def empty-pkg (set-rank c/empty-pkg 0))
+
+(defmacro run* [[& vars] & gs]
+  `(c/cK-reify (c/take* ((fresh [~@vars]
+                           ~@gs)
+                         empty-pkg))))
+
+(defmacro run [n [& vars] & gs]
+  `(take ~n (run* ~vars ~@gs)))
+
+(def == c/==)
+(def != c/!=)
+
