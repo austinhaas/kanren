a016055cafd2f9d2fc3cb7807df083acfd14b6ba
Austin Haas <austin@pettomato.com>
2014-01-19 20:46:30 -0800

    llist.clj: Remove comment.

--- src/com/pettomato/kanren/llist.clj
+++ src/com/pettomato/kanren/llist.clj
@@ -1,10 +1,5 @@
 (ns com.pettomato.kanren.llist)
 
-;; We need a list data structure to use within the search. At the very
-;; least, it should have the semantics of Scheme's cons. Externally
-;; supplied sequences would need to be converted to the internal
-;; representation. Users can reify it to a vector, if desired.
-
 (def empty-llist nil)
 
 (defn lcons [a d]
