c14c9cb4809ce465c24cfa3308fe2e055a27e2b9
Austin Haas <austin@pettomato.com>
2014-01-21 20:41:55 -0800

    Add more examples to demo.clj.

--- src/com/pettomato/kanren/demo.clj
+++ src/com/pettomato/kanren/demo.clj
@@ -1,10 +1,10 @@
 (ns com.pettomato.kanren.demo
   (:refer-clojure :exclude [==])
   (:require
-   [com.pettomato.kanren.cKanren :as c :refer (!= == fresh conde run* run)]
+   [com.pettomato.kanren.cKanren :as c :refer (!= == fresh conde run* run unit)]
    [com.pettomato.kanren.rKanren :as r]
    [com.pettomato.kanren.llist :refer (empty-llist llist llist* llist->seq)]
-   [com.pettomato.kanren.goals :refer (emptyo conso membero appendo nonmembero)]))
+   [com.pettomato.kanren.goals :refer (emptyo conso membero appendo nonmembero succeed anyo alwayso)]))
 
 #_(take 5
     (run* [q]
@@ -43,6 +43,53 @@
 
 #_(r/run* [q]
     (r/condr
-     [3 (r/== q 'second)]
-     [5 (r/== q 'third)]
+     [2 (r/== q 'second)]
+     [3 (r/== q 'third)]
      [1 (r/== q 'first)]))
+
+(defn recur-e [e]
+  (r/fresh [a b]
+    (r/conde
+      [(r/== e ['x])]
+      [(r/== e ['b a]) (recur-e a)]
+      [(r/== e ['a b]) (recur-e b)])))
+
+#_(r/run 5 [q] (recur-e q))
+
+(defn recur-r [e]
+  (r/fresh [a b]
+    (r/condr
+      [10 (r/== e ['x])]
+      [ 4 (r/== e ['b a]) (recur-r a)]
+      [ 2 (r/== e ['a b]) (recur-r b)])))
+
+#_(r/run 5 [q] (recur-r q))
+
+#_(run 5 [q]
+    (nonmembero q (llist* 1 2 3))
+    (conde
+      [(== q 1)]
+      [(== q 4)]))
+
+#_(run 10 [q]
+    (conde
+      [alwayso
+       (conde
+         [alwayso])]
+      [(== q 3)]))
+
+#_(frequencies
+   (run 100 [q]
+     (conde
+       [alwayso (conde
+                  [alwayso (== q 1)]
+                  [alwayso (== q 2)]
+                  [alwayso (== q 3)])]
+       [alwayso (conde
+                  [alwayso (== q 10)]
+                  [alwayso (== q 20)]
+                  [alwayso (== q 30)])]
+       [alwayso (conde
+                  [alwayso (== q 100)]
+                  [alwayso (== q 200)]
+                  [alwayso (== q 300)])])))
