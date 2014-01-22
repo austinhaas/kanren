22a5fe3f602e03595ec454a33350c80d74bc6b19
Austin Haas <austin@pettomato.com>
2014-01-22 13:57:41 -0800

    rKanren: Fixed.

--- src/com/pettomato/kanren/rKanren/rKanren.clj
+++ src/com/pettomato/kanren/rKanren/rKanren.clj
@@ -5,74 +5,103 @@
 
 (def lvar c/lvar)
 
-(def unit c/unit)
-(def mzero c/mzero)
+(def mzero  c/mzero)
+(def unit   c/unit)
 (def choice c/choice)
-(def mzero? c/mzero?)
-(def unit? c/unit?)
 
-(defn get-rank [a-inf]
-  (get (meta a-inf) ::rank -1))
+(def mzero?  c/mzero?)
+(def unit?   c/unit?)
+(def rdelay? fn?)
+(defn choice? [x] (and (vector? x) (= (count x) 2)))
+
+(defmacro case-inf
+  [e _ e0 [f'] e1 [a'] e2 [a f] e3]
+  `(let [a# ~e]
+     (cond
+      (mzero? a#)  ~e0
+      (rdelay? a#) (let [~f' a#] ~e1)
+      (unit? a#)   (let [~a' a#] ~e2)
+      (choice? a#) (let [[~a ~f] a#] ~e3)
+      :else (throw (Error. (str "Unknown type of a-inf:" a#) )))))
 
 (defn set-rank [a-inf r]
+  (assert a-inf)
   (vary-meta a-inf assoc ::rank r))
 
-(defn inc-rank [a-inf]
-  (and a-inf
-       (let [r (get-rank a-inf)]
-         (vary-meta a-inf assoc ::rank (inc r)))))
+(defn get-rank [a-inf]
+  (case-inf a-inf
+            []    -1
+            [f]   (get (meta f) ::rank)
+            [a]   (get (meta a) ::rank)
+            [a f] -1))
 
-(defn add-rank [a-inf r]
-  (vary-meta a-inf update-in [::rank] + r))
+(defn add-rank [a r]
+  (assert a)
+  (vary-meta a update-in [::rank] + r))
+
+(defn inc-rank- [stream-or-subst]
+  (vary-meta stream-or-subst update-in [::rank] inc))
+
+(defn inc-rank [a-inf]
+  (case-inf a-inf
+            []    mzero
+            [f]   (inc-rank- f)
+            [a]   (inc-rank- a)
+            [a f] (choice (inc-rank a) (inc-rank f))))
 
 (defmacro rdelay [r & body]
   `(set-rank
     (fn [] ~@body)
     ~r))
 
-(defn rdelay? [d] (fn? d))
+(defn rforce [a-inf]
+  (case-inf a-inf
+            []    mzero
+            [f]   (f)
+            [a]   a
+            [a f] a-inf))
 
-(defn rforce [d]
-  (assert (rdelay? d) (str d))
-  (d))
-
-(defmacro case-inf
+(defmacro case-inf+
   [e _ e0 [f'] e1 [a'] e2 [a f] e3]
   `(let [a# ~e]
      (cond
       (mzero? a#)  (inc-rank ~e0)
       (rdelay? a#) (let [~f' a#] (inc-rank ~e1))
       (unit? a#)   (let [~a' a#] (inc-rank ~e2))
-      :else        (let [[~a ~f] a#] (inc-rank ~e3)))))
+      (choice? a#) (let [[~a ~f] a#] (inc-rank ~e3))
+      :else (throw (Error. (str "Unknown type of a-inf:" a#) )))))
 
 (defn mplus [a-inf f]
-  (case-inf a-inf
-            []     f
-            [f']   (let [f'-rank (get-rank f')
-                         f-rank (get-rank f)]
-                     (if (< f'-rank f-rank)
-                       (rdelay f'-rank (mplus (rforce f') f))
-                       (rdelay f-rank  (mplus f (rforce f')))))
-            [a]    (choice a f)
-            [a f'] (choice a
-                           (let [f'-rank (get-rank f')
-                                 f-rank (get-rank f)]
-                             (if (< f'-rank f-rank)
-                               (rdelay f'-rank (mplus (rforce f') f))
-                               (rdelay f-rank  (mplus f  (rforce f'))))))))
+  (case-inf+ a-inf
+             []     f
+             [f']   (let [f'-rank (get-rank f')
+                          f-rank (get-rank f)]
+                      (if (< f'-rank f-rank)
+                        (rdelay f'-rank (mplus (rforce f') f ))
+                        (rdelay f-rank  (mplus (rforce f ) f'))))
+             [a]    (choice a f)
+             [a f'] (choice a
+                            (let [f'-rank (get-rank f')
+                                  f-rank (get-rank f)]
+                              (if (< f'-rank f-rank)
+                                (rdelay f'-rank (mplus (rforce f') f ))
+                                (rdelay f-rank  (mplus (rforce f ) f')))))))
 
 (defn bind [a-inf g]
-  (case-inf a-inf
-            []     mzero
-            [f]    (rdelay (get-rank f) (bind (rforce f) g))
-            [a]    (g a)
-            [a f]  (mplus (g a) (rdelay (get-rank f) (bind f g)))))
+  (case-inf+ a-inf
+             []     mzero
+             [f]    (rdelay (get-rank f) (bind (rforce f) g))
+             [a]    (g a)
+             [a f]  (mplus (g a) (rdelay (get-rank f) (bind (rforce f) g)))))
+
+(defn min-rank [as]
+  (apply min (map get-rank as)))
 
 (defmacro mplus*
   ([e] e)
   ([e & es]
-     `(let [min-rank# (apply min (map get-rank '~es))
-            e-rank# (get-rank ~e)]
+     `(let [e-rank# (get-rank ~e)
+            min-rank# (min-rank [~@es])]
         (if (< e-rank# min-rank#)
           (mplus ~e (rdelay min-rank# (mplus* ~@es)))
           (mplus (rdelay min-rank# (mplus* ~@es))
@@ -122,17 +151,12 @@
 
 (def reify-var c/reify-var)
 
-(defn pull [x]
-  (if (rdelay? x)
-    (recur (rforce x))
-    x))
-
 (defn take* [f]
-  (case-inf f
-            []    ()
-            [f]   (take* (pull f))
-            [a]   (cons a ())
-            [a f] (cons a (lazy-seq (take* f)))))
+  (case-inf (rforce f)
+            []     ()
+            [f']   (recur f')
+            [a]    (cons a ())
+            [a f'] (cons a (lazy-seq (take* f')))))
 
 (def empty-pkg (set-rank c/empty-pkg 0))
 
