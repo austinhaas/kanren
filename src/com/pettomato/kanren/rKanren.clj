f831631436a65406e3f4b2c1341559a81571b127
Austin Haas <austin@pettomato.com>
2014-01-21 20:43:59 -0800

    Finish rKanren.

--- src/com/pettomato/kanren/rKanren.clj
+++ src/com/pettomato/kanren/rKanren.clj
@@ -4,10 +4,12 @@
    [com.pettomato.kanren.muKanren :as mu]
    [com.pettomato.kanren.cKanren :as c]))
 
-(def pair? mu/pair?)
+(def lvar mu/lvar)
 
 (def mzero mu/mzero)
 (def choice mu/choice)
+(def mzero? mu/mzero?)
+(def unit? mu/unit?)
 
 (defn get-rank [a-inf]
   (get (meta a-inf) ::rank -1))
@@ -23,98 +25,118 @@
 (defn add-rank [a-inf r]
   (vary-meta a-inf update-in [::rank] + r))
 
+(defmacro rdelay [r & body]
+  `(set-rank
+    (fn [] ~@body)
+    ~r))
+
+(defn rdelay? [d] (fn? d))
+
+(defn rforce [d]
+  (assert (rdelay? d))
+  (d))
+
 (defmacro case-inf
   [e _ e0 [f'] e1 [a'] e2 [a f] e3]
   `(let [a# ~e]
      (cond
-      (false? a#) (inc-rank ~e0)
-      (fn? a#) (let [~f' a#] (inc-rank ~e1))
-      (not (and (pair? a#) (fn? (second a#))))
-      (let [~a' a#]
-        (inc-rank ~e2))
-      :else (let [~a (first a#)
-                  ~f (second a#)]
-              (inc-rank ~e3)))))
+      (mzero? a#)  (inc-rank ~e0)
+      (rdelay? a#) (let [~f' a#] (inc-rank ~e1))
+      (unit? a#)   (let [~a' a#] (inc-rank ~e2))
+      :else        (let [[~a ~f] a#] (inc-rank ~e3)))))
 
 (defn mplus [a-inf f]
   (case-inf a-inf
-            []     f
+            []     (rforce f)
             [f']   (let [f'-rank (get-rank f')
-                         f-rank  (get-rank f)]
+                         f-rank (get-rank f)]
                      (if (< f'-rank f-rank)
-                       (set-rank (fn [] (mplus (f') f))
-                                 f'-rank)
-                       (set-rank (fn [] (mplus (f)  f'))
-                                 f-rank)))
+                       (rdelay f'-rank (mplus (rforce f') f))
+                       (rdelay f-rank  (mplus f (rforce f')))))
             [a]    (choice a f)
             [a f'] (choice a
                            (let [f'-rank (get-rank f')
-                                 f-rank  (get-rank f)]
+                                 f-rank (get-rank f)]
                              (if (< f'-rank f-rank)
-                               (set-rank (fn [] (mplus (f') f))
-                                         f'-rank)
-                               (set-rank (fn [] (mplus (f)  f'))
-                                         f-rank))))))
+                               (rdelay f'-rank (mplus (rforce f') f))
+                               (rdelay f-rank  (mplus f  (rforce f'))))))))
 
 (defn bind [a-inf g]
   (case-inf a-inf
-            []    mzero
-            [f]   (set-rank (fn [] (bind (f) g))
-                            (get-rank f))
-            [a]   (g a)
-            [a f] (mplus (g a) (set-rank (fn [] (bind (f) g))
-                                         (get-rank f)))))
+            []     mzero
+            [f]    (rdelay (get-rank f) (bind (rforce f) g))
+            [a]    (g a)
+            [a f]  (mplus (g a) (rdelay (get-rank f) (bind f g)))))
 
-(defn disj [g1 g2] (fn [a] (mplus (g1 a) (g2 a))))
+(defn foo
+  ([x] 1)
+  ([x & xs] 2))
 
-(defn conj [g1 g2] (fn [a] (bind (g1 a) g2)))
+(defmacro mplus*
+  ([e] e)
+  ([e & es]
+     `(let [min-rank# (apply min (map get-rank '~es))
+            e-rank# (get-rank ~e)]
+        (if (< e-rank# min-rank#)
+          (mplus ~e (rdelay min-rank# (mplus* ~@es)))
+          (mplus (rdelay min-rank# (mplus* ~@es))
+                 (rdelay e-rank# ~e))))))
 
-(defmacro Zzz [g]
-  `(fn [a#]
-     (set-rank
-      (fn [] (~g a#))
-      (get-rank a#))))
-
-(defmacro disj+
-  ([g] `(Zzz ~g))
-  ([g & gs]
-     `(disj (Zzz ~g) (disj+ ~@gs))))
-
-(defmacro conj+
-  ([g] `(Zzz ~g))
-  ([g & gs]
-     `(conj (Zzz ~g) (conj+ ~@gs))))
+(defmacro bind*
+  ([e] e)
+  ([e g & gs] `(bind* (bind ~e ~g) ~@gs)))
 
 (defmacro conde
   [& clauses]
-  `(disj+
-    ~@(for [c clauses]
-        `(conj+ ~@c))))
+  (let [a (gensym)
+        a' (gensym)]
+    `(fn [~a]
+       (rdelay
+        (get-rank ~a)
+        (let [~a' (inc-rank ~a)]
+          (mplus*
+           ~@(for [[g & gs] clauses]
+               `(bind* (~g ~a') ~@gs))))))))
 
 (defmacro condr
   [& clauses]
-  `(disj+
-    ~@(for [[r & c] clauses]
-        `(fn [a#]
-           ((conj+ ~@c) (add-rank a# ~r))))))
-
-(defn call:fresh [f]
-  (fn [{:keys [i] :as pkg}]
-    (let [pkg' (update-in pkg [:i] inc)]
-      ((f (c/lvar i)) pkg'))))
+  (let [a (gensym)
+        a' (gensym)]
+    `(fn [~a]
+       (rdelay
+        (get-rank ~a)
+        (let [~a' (inc-rank ~a)]
+          (mplus*
+           ~@(for [[r g & gs] clauses]
+               `(bind* (~g (add-rank ~a' ~r)) ~@gs))))))))
 
 (defmacro fresh
-  [[& vars] & gs]
-  (if (empty? vars)
-    `(conj+ ~@gs)
-    `(call:fresh (fn [~(first vars)] (fresh ~(rest vars) ~@gs)))))
+  [[& vars] g & gs]
+  (let [n (count vars)
+        a (gensym)
+        a' (gensym)
+        c (gensym)]
+    `(fn [~a]
+       (rdelay
+        (get-rank ~a)
+        (let [~c (:i ~a)
+              ~@(apply concat (map-indexed (fn [i v] `[~v (lvar (+ ~c ~i))]) vars))
+              ~a' (update-in ~a [:i] + ~n)]
+          (bind* (~g ~a') ~@gs))))))
+
+(defn take* [f]
+  (case-inf f
+            []    ()
+            [f]   (take* (f))
+            [a]   (cons a ())
+            [a f] (cons a (lazy-seq (take* f)))))
 
 (def empty-pkg (set-rank c/empty-pkg 0))
 
 (defmacro run* [[& vars] & gs]
-  `(c/cK-reify (c/take* ((fresh [~@vars]
-                           ~@gs)
-                         empty-pkg))))
+  `(c/cK-reify (take* ((fresh [~@vars]
+                         ~@gs)
+                       empty-pkg))))
 
 (defmacro run [n [& vars] & gs]
   `(take ~n (run* ~vars ~@gs)))
