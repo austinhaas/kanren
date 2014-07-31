ae0153fa5488ab734449f7cae13ff9fa8e8d2e5b
Austin Haas <austin@pettomato.com>
2014-07-31 10:06:59 -0700

    Fix bug in build-oc and simplify it.

--- src/clj/com/pettomato/kanren/cKanren/build_oc.clj
+++ src/clj/com/pettomato/kanren/cKanren/build_oc.clj
@@ -1,10 +1,6 @@
 (ns com.pettomato.kanren.cKanren.build-oc)
 
-(defmacro build-aux-oc [op args zs args2]
-  (if (empty? args)
-    `(let [~@(interleave zs args)]
-       (list (~op ~@zs) (quote ~op) ~@zs))
-    `(build-aux-oc ~op ~(rest args) ~(conj zs (first args)) ~args)))
-
 (defmacro build-oc [op & args]
-  `(build-aux-oc ~op ~args [] ~args))
+  (let [vars (repeatedly (count args) gensym)]
+   `(let [~@(interleave vars args)]
+      (list (~op ~@vars) (quote ~op) ~@vars))))
